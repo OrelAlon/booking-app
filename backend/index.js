220df279f39c7bc871ee6fda7d11c7698009cdec
OrelAlon <orela231089@gmail.com>
2022-07-21 18:15:16 +0300

    add mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
 
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, () => {
+      console.log("Connected to MongoDB !!");
+    });
+  } catch (error) {
+    throw error;
+  }
+};
 app.listen(8000, () => {
+  connect();
   console.log("Connected to backend !!");
 });
