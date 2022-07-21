60837cad9ba52d276057a4cae5e56f32ae01c3a6
OrelAlon <orela231089@gmail.com>
2022-07-21 20:06:15 +0300

    finish route hotel

---
+++ backend/controllers/hotelController.js
@@ -0,0 +1,65 @@
+const Hotel = require("../models/Hotel");
+
+//
+const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+//
+const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+//
+const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+
+    res.status(200).json("Hotel Delete");
+  } catch (err) {
+    next(err);
+  }
+};
+
+//
+const getHotel = async (req, res, next) => {
+  try {
+    const getHotel = await Hotel.findById(req.params.id);
+
+    res.status(200).json(getHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+//
+const getHotels = async (req, res, next) => {
+  try {
+    const getHotels = await Hotel.find();
+
+    res.status(200).json(getHotels);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { createHotel, updateHotel, deleteHotel, getHotel, getHotels };
--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,69 +1,29 @@
 const express = require("express");
+const {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+} = require("../controllers/hotelController");
+
 const Hotel = require("../models/Hotel");
 
 const router = express.Router();
 
 // Create Hotel
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.post("/", createHotel);
 
 // Update Hotel
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-
-    res.status(200).json(updatedHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.put("/:id", updateHotel);
 
 // Delete Hotel
-router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-
-    res.status(200).json("Hotel Delete");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.delete("/:id", deleteHotel);
 
 // Get Hotel
-router.get("/:id", async (req, res) => {
-  try {
-    const getHotel = await Hotel.findById(req.params.id);
-
-    res.status(200).json(getHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/:id", getHotel);
 
 // Get All Hotels
-router.get("/", async (req, res, next) => {
-  // const failed = true
-  // if (failed) return next(handleError(401,'You are not authenticated'))
-  try {
-    const getHotels = await Hotel.find();
-
-    res.status(200).json(getHotels);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", getHotels);
 
 module.exports = router;
