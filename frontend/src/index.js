a9ca0ab8662c151b1f4f5c7da62f6f30623ef8cb
OrelAlon <orela231089@gmail.com>
2022-07-31 10:14:30 +0300

    add context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 // import { AuthContextProvider } from "./context/AuthContext";
-// import { SearchContextProvider } from "./context/SearchContext";
+import { SearchContextProvider } from "./context/SearchContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <AuthContextProvider>
-      <SearchContextProvider> */}
-    <App />
-    {/* </SearchContextProvider>
-    </AuthContextProvider> */}
+    {/* <AuthContextProvider> */}
+    <SearchContextProvider>
+      <App />
+    </SearchContextProvider>
+    {/* </AuthContextProvider> */}
   </React.StrictMode>
 );
--- frontend/src/pages/hotel/Hotel.jsx
+++ frontend/src/pages/hotel/Hotel.jsx
@@ -102,13 +102,14 @@ const Hotel = () => {
             <h1 className='hotelTitle'>{data.name}</h1>
             <div className='hotelAddress'>
               <FontAwesomeIcon icon={faLocationDot} />
-              <span>Elton St 125 New york</span>
+              <span>{data.address}</span>
             </div>
             <span className='hotelDistance'>
-              Excellent location – 500m from center
+              Excellent location – {data.distance}m from center
             </span>
             <span className='hotelPriceHighlight'>
-              Book a stay over $114 at this property and get a free airport taxi
+              Book a stay over ${data.cheapestPrice} at this property and get a
+              free airport taxi
             </span>
             <div className='hotelImages'>
               {photos.map((photo, i) => (
@@ -124,21 +125,8 @@ const Hotel = () => {
             </div>
             <div className='hotelDetails'>
               <div className='hotelDetailsTexts'>
-                <h1 className='hotelTitle'>Stay in the heart of City</h1>
-                <p className='hotelDesc'>
-                  Located a 5-minute walk from St. Florian's Gate in Krakow,
-                  Tower Street Apartments has accommodations with air
-                  conditioning and free WiFi. The units come with hardwood
-                  floors and feature a fully equipped kitchenette with a
-                  microwave, a flat-screen TV, and a private bathroom with
-                  shower and a hairdryer. A fridge is also offered, as well as
-                  an electric tea pot and a coffee machine. Popular points of
-                  interest near the apartment include Cloth Hall, Main Market
-                  Square and Town Hall Tower. The nearest airport is John Paul
-                  II International Kraków–Balice, 16.1 km from Tower Street
-                  Apartments, and the property offers a paid airport shuttle
-                  service.
-                </p>
+                <h1 className='hotelTitle'>{data.title}</h1>
+                <p className='hotelDesc'>{data.desc}</p>
               </div>
               <div className='hotelDetailsPrice'>
                 <h1>Perfect for a 9-night stay!</h1>
