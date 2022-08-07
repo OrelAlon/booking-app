4df3d355e06338af666e9dd19dc8c2a31705c457
OrelAlon <orela231089@gmail.com>
2022-08-07 15:13:09 +0300

    heroku

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const app = express();
+const path = require("path");
 
 dotenv.config();
 
@@ -40,6 +41,20 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+//
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMsg = err.message || "Something crashhh";
