2bce6d96d7a419b20306b584bae85ab4d8f01eba
OrelAlon <orela231089@gmail.com>
2022-08-14 16:23:22 +0300

    make it better

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -9,7 +9,7 @@ import Header from "../../components/header/Header";
 import SearchItem from "../../components/searchItem/SearchItem";
 
 import "./list.css";
-
+// f12
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
